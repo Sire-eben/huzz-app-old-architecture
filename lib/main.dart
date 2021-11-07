2846c2e74a6c603f4d8d7d9d92d1e549d9acd422
Israel Akpan <isrealakpan296@gmail.com>
2021-11-07 06:49:39 +0100

    [WIP] Project architecture setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:huzz/core/config/providers/app_providers.dart';
 import 'package:huzz/core/routes/app_router.dart';
 import 'package:huzz/core/routes/app_routes.dart';
 import 'package:provider/provider.dart';
-
-import 'core/config/providers/app_providers.dart';
 
 class HuzzApp extends StatefulWidget {
   const HuzzApp({Key key}) : super(key: key);
