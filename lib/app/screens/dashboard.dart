03f15882236003bf8251f6c2d1df7b957303d335
Adeyeye Adedamola <damscozyblog@gmail.com>
2021-11-28 00:02:06 +0100

    dashbord

--- lib/app/screens/dashboard.dart
+++ lib/app/screens/dashboard.dart
@@ -1,7 +1,9 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:huzz/app/screens/users%20screen/home.dart';
 import 'package:huzz/colors.dart';
-import 'home.dart';
+import 'users screen/team.dart';
+import 'users screen/inventry.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -11,53 +13,92 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  int index = 0;
+  late List<Widget> _screens;
+
+  @override
+  void initState() {
+    _screens = [
+      //teams
+      Teams(),
+
+      //inventry
+      InventaryTabView(),
+
+      //home
+      HomePage(),
+
+      //invoice
+      //Invoice(),
+      InventaryTabView(),
+
+      //more
+      //More(),
+      InventaryTabView(),
+    ];
+    super.initState();
+  }
+
+  int _selectedIndex = 2;
+  // ignore: unused_element
+  void _selectPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   final inactiveColor = Colors.grey;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home'),
-      ),
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: index,
-        items: <BottomNavyBarItem>[
-          BottomNavyBarItem(
-              icon: Icon(Icons.people),
-              title: Text('Teams'),
-              activeColor: AppColor().backgroundColor,
-              inactiveColor: inactiveColor),
-          BottomNavyBarItem(
-              icon: Icon(Icons.inventory),
-              title: Text('Inventory'),
-              activeColor: AppColor().backgroundColor,
-              inactiveColor: inactiveColor),
-          BottomNavyBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-              activeColor: AppColor().backgroundColor,
-              inactiveColor: inactiveColor),
-          BottomNavyBarItem(
-              icon: Icon(Icons.receipt),
-              title: Text('Invoice'),
-              activeColor: AppColor().backgroundColor,
-              inactiveColor: inactiveColor),
-          BottomNavyBarItem(
-              icon: Icon(Icons.dashboard),
-              title: Text('More'),
-              activeColor: AppColor().backgroundColor,
-              inactiveColor: inactiveColor)
-        ],
-        onItemSelected: (index) => setState(() => this.index = index),
+      body: IndexedStack(index: _selectedIndex, children: _screens),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          border: Border.all(
+            color: Color(0xffC3C3C3).withOpacity(0.3),
+            width: 2,
+          ),
+        ),
+        child: BottomNavyBar(
+          backgroundColor: Color(0xffF5F5F5),
+          selectedIndex: _selectedIndex,
+          items: <BottomNavyBarItem>[
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                icon: Icon(Icons.people),
+                title: Text('Teams'),
+                activeColor: AppColor().backgroundColor,
+                inactiveColor: inactiveColor),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                icon: Icon(Icons.inventory),
+                title: Text('Inventory'),
+                activeColor: AppColor().backgroundColor,
+                inactiveColor: inactiveColor),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+                activeColor: AppColor().backgroundColor,
+                inactiveColor: inactiveColor),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                icon: Icon(Icons.receipt),
+                title: Text('Invoice'),
+                activeColor: AppColor().backgroundColor,
+                inactiveColor: inactiveColor),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                icon: Icon(Icons.dashboard),
+                title: Text('More'),
+                activeColor: AppColor().backgroundColor,
+                inactiveColor: inactiveColor)
+          ],
+          onItemSelected: (index) =>
+              setState(() => this._selectedIndex = index),
+        ),
       ),
     );
-  }
-
-  Widget buildPages() {
-    switch (index) {
-      case 0:
-      default:
-        return Home();
-    }
   }
 }
--- lib/app/screens/users screen/team.dart
+++ lib/app/screens/users screen/team.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class Teams extends StatefulWidget {
+  const Teams({Key? key}) : super(key: key);
 
   @override
-  _HomeState createState() => _HomeState();
+  _TeamsState createState() => _TeamsState();
 }
 
-class _HomeState extends State<Home> {
+class _TeamsState extends State<Teams> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text('Home'),
+          child: Text('Teams'),
         ),
       ),
     );
