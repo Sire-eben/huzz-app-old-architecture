d27aa55997d70bc2921a321b11bee3529700900e
michtimiael <michtimiael@gmail.com>
2022-03-02 12:06:30 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:get/get.dart';
 import 'package:huzz/app/appbinding.dart';
 import 'package:huzz/colors.dart';
