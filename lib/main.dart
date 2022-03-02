45613489534c15907be6d688216dcfe826d26c8a
michtimiael <michtimiael@gmail.com>
2022-03-02 12:10:42 +0100

    firebase setup for android completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:huzz/app/appbinding.dart';
@@ -6,8 +7,9 @@ import 'package:huzz/core/constants/app_pallete.dart';
 import 'package:huzz/core/routes/app_router.dart';
 import 'app/screens/splashscreen.dart';
 
-void main() {
-  //  FlutterContacts.requestPermission();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(HuzzApp());
 }
 
