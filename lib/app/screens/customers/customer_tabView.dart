29725ba7f21ce2fc34cfec824cec30e9d83d82df
michtimiael <michtimiael@gmail.com>
2022-03-13 20:57:05 +0100

    info for manage customer fixed

--- lib/app/screens/customers/customer_tabView.dart
+++ lib/app/screens/customers/customer_tabView.dart
@@ -1,9 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:huzz/app/screens/customers/merchants/merchants.dart';
-
 import '../../../colors.dart';
 import 'customer/customers.dart';
+
+class ManageCustomerInformationDialog extends StatelessWidget {
+  const ManageCustomerInformationDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.info_outline_rounded,
+          size: 27,
+        ),
+        SizedBox(height: 7),
+        Text(
+          'Your customers are the people you sell products or services to.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 14,
+            fontFamily: "InterRegular",
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 class CustomerTabView extends StatefulWidget {
   const CustomerTabView({Key? key}) : super(key: key);
@@ -50,31 +80,69 @@ class _CustomerTabViewState extends State<CustomerTabView> {
                   ),
                 ),
                 SizedBox(width: 4),
-
-                //Tool tip
-                Tooltip(
-                  triggerMode: TooltipTriggerMode.tap,
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(color: Colors.black38, blurRadius: 10)
-                      ]),
-                  textStyle: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontFamily: 'InterRegular',
-                      fontSize: 10,
-                      color: Colors.black),
-                  preferBelow: false,
-                  message:
-                      'Your customers are the\npeople you sell products\nor services to',
-                  child: SvgPicture.asset(
-                    "assets/images/info.svg",
-                    height: 20,
-                    width: 20,
+                GestureDetector(
+                  onTap: () {
+                    Platform.isIOS
+                        ? showCupertinoDialog(
+                            context: context,
+                            barrierDismissible: true,
+                            builder: (context) => CupertinoAlertDialog(
+                              content: ManageCustomerInformationDialog(),
+                              actions: [
+                                CupertinoButton(
+                                  child: Text("OK"),
+                                  onPressed: () => Get.back(),
+                                ),
+                              ],
+                            ),
+                          )
+                        : showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              content: ManageCustomerInformationDialog(),
+                              actions: [
+                                CupertinoButton(
+                                  child: Text("OK"),
+                                  onPressed: () => Get.back(),
+                                ),
+                              ],
+                            ),
+                          );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 4.0, top: 2.0),
+                    child: SvgPicture.asset(
+                      "assets/images/info.svg",
+                      height: 20,
+                      width: 20,
+                    ),
                   ),
                 ),
+
+                //Tool tip
+                // Tooltip(
+                //   triggerMode: TooltipTriggerMode.tap,
+                //   padding: EdgeInsets.all(6),
+                //   decoration: BoxDecoration(
+                //       borderRadius: BorderRadius.circular(10),
+                //       color: Colors.white,
+                //       boxShadow: [
+                //         BoxShadow(color: Colors.black38, blurRadius: 10)
+                //       ]),
+                //   textStyle: TextStyle(
+                //       fontWeight: FontWeight.w500,
+                //       fontFamily: 'InterRegular',
+                //       fontSize: 10,
+                //       color: Colors.black),
+                //   preferBelow: false,
+                //   message:
+                //       'Your customers are the\npeople you sell products\nor services to',
+                //   child: SvgPicture.asset(
+                //     "assets/images/info.svg",
+                //     height: 20,
+                //     width: 20,
+                //   ),
+                // ),
               ],
             ),
             bottom: PreferredSize(
