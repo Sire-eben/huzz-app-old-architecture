13e3b74bc015cb763009cbf1f02713e64fc8a246
JPrince-E <adebayoea1@gmail.com>
2022-12-13 00:43:41 +0100

    Enoch: Added telegram link to support screen

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -2,12 +2,17 @@ mixin AppStrings {
   static const appWebName = 'Huzz';
   static const helpAndSupport = 'Help and Support';
   static const connectOnWhatsApp = 'Connect on WhatsApp';
+  static const connectOnTelegram = 'Connect on Telegram';
   static const mail = 'Mail';
   static const faq = 'FAQ';
   static const termsOfUse = 'Terms Of Use';
   static const privacyPolicy = 'Privacy Policy';
 
-  static const continueToProceed = 'Please click continue if you want to proceed';
+  static const continueToProceed =
+      'Please click continue if you want to proceed';
 
-  static const huzzWhatsAppContactURI = 'https://api.whatsapp.com/send?phone=+2348132894616';
+  static const huzzWhatsAppContactURI =
+      'https://api.whatsapp.com/send?phone=+2348132894616';
+
+  static const huzzTelegramContactURI = 'https://t.me/+_Of_Mc-Yet0wZmRk';
 }
